62757bac2cf6467c0d028dc82f530314b1ef77e2
Albert Nikolli <albert_nikolli@icloud.com>
2024-12-25 23:01:31 +0100

    changed the reciever

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,7 +4,7 @@ class NotificationMailer < ApplicationMailer
   def new_application_email(application)
     @application = application
     mail(
-      to: 'albert_nikolli@icloud.com', # Replace with the desired recipient email
+      to: 's.kiblade@hotmil.it', # Replace with the desired recipient email
       subject: 'New Application Submitted'
     )
   end
