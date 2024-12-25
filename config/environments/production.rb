221108bc9276cc82267662652948f42c60a2c1c4
Albert Nikolli <albert_nikolli@icloud.com>
2024-12-25 14:39:57 +0100

    took care of the mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,21 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://www.concours-entrelace.com/" }
+  config.action_mailer.default_url_options = { host: "https://www.concours-entrelace.com" }
   # Settings specified here will take precedence over those in config/application.rb.
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address:         "smtp.gmail.com",
+    port:            587,
+    domain:          "example.com",
+    user_name:       ENV["SMTP_EMAIL"],
+    password:        ENV["SMTP_PASSWORD"],
+    authentication:  "plain",
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
@@ -78,7 +91,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
