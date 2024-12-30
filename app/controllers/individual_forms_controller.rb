e37b8be1dd426f05c59807863d296ab3c18bd45f
Albert Nikolli <albert_nikolli@icloud.com>
2024-12-30 19:31:51 +0100

    took off the line for the mail

--- app/controllers/individual_forms_controller.rb
+++ app/controllers/individual_forms_controller.rb
@@ -109,7 +109,6 @@ class IndividualFormsController < ApplicationController
 
     if @individual_form.save
       notice_message = I18n.t('individual_form.create.success')
-      NotificationMailer.new_application_email(@individual_form).deliver_now
       redirect_to confirmation_form_path, notice: notice_message
     else
       alert_message1 = I18n.t('individual_form.create.error')
