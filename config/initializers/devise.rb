68f4d81fe7e83062d2ecf3837640758e598122fc
Albert Nikolli <albert_nikolli@icloud.com>
2025-01-12 23:41:49 +0100

    changed the initializer email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'concours.entrelace@gmail.com'
+  config.mailer_sender = 'no-reply@concours-entrelace.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
