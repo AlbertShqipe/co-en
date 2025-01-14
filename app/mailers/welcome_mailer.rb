31f487c3d301d11df2e86327de1696a5b76aaab9
Albert Nikolli <albert_nikolli@icloud.com>
2025-01-14 13:08:47 +0100

    testing the welcome email

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,6 +2,10 @@ class WelcomeMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'https://www.concours-entrelace.com/users/sign_in' # Adjust to your login URL
+
+    # Attach the image
+    attachments.inline['LogoFullOrange1.png'] = File.read(Rails.root.join('app/assets/images/LogoFullOrange1.png'))
+
     mail(
       to: @user.email,
       subject: 'Bienvenue au Concours Entrelacé!'
