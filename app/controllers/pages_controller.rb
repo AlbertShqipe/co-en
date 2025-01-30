37bbd1de3607e49b50ab4f012f0d90efc1a54d9d
Albert Nikolli <albert_nikolli@icloud.com>
2025-01-30 12:42:11 +0100

    setting the duos to be able to get the assets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
   end
 
   def test
-    @users = User.includes(:duo).order(:name, :last_name)
+    @users = User.includes(:duos).order(:name)
     # Code that runs only in development since it charges the assets uploaded in development
     @results_dev = Cloudinary::Api.resources(type: "upload", prefix: "development", max_results: 500)['resources']
 
