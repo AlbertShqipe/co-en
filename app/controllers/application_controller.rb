1cd9c83c9de74f28a48568775f6a745a2ff307e5
Albert Nikolli <albert_nikolli@icloud.com>
2025-08-22 13:39:20 +0200

    set few changes in relation to the locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,17 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = if %w[en fr].include?(params[:locale])
+      params[:locale]
+    else
+      I18n.default_locale
+    end
   end
 
-  # Optional: To make sure URLs include the locale
+  # Include locale in all generated URLs as a path segment
   def default_url_options
     { locale: I18n.locale }
+    # If you prefer to *hide* the segment for the default locale, use:
+    # I18n.locale == I18n.default_locale ? {} : { locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,58 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|fr/ do
+    # Different pages for the website
+    root to: "pages#home"
+    get 'regulation' => 'pages#regulation'
+    get 'media' => 'pages#media'
+    get 'who_we_are' => 'pages#who_we_are'
+    get 'practical_info' => 'pages#practical_info'
+    get 'contact' => 'pages#contact'
+    get 'our_partners' => 'pages#our_partners'
+    get 'apply' => 'pages#apply'
+    get 'profile' => 'pages#profile'
+    get 'admin' => 'pages#admin'
+    get "messages" => 'pages#messages'
+    get "confirmation_form" => 'pages#confirmation_form'
+    get 'test' => 'pages#test'
 
-  # Different pages for the website
-  root to: "pages#home"
-  get 'regulation' => 'pages#regulation'
-  get 'media' => 'pages#media'
-  get 'who_we_are' => 'pages#who_we_are'
-  get 'practical_info' => 'pages#practical_info'
-  get 'contact' => 'pages#contact'
-  get 'our_partners' => 'pages#our_partners'
-  get 'apply' => 'pages#apply'
-  get 'profile' => 'pages#profile'
-  get 'admin' => 'pages#admin'
-  get "messages" => 'pages#messages'
-  get "confirmation_form" => 'pages#confirmation_form'
-  get 'test' => 'pages#test'
+    # Popup
+    get 'dismiss_welcome_popup', to: 'pages#dismiss_welcome_popup'
 
-  # Popup
-  get 'dismiss_welcome_popup', to: 'pages#dismiss_welcome_popup'
+    # Devise authentication routes
+    devise_for :users, controllers: { registrations: 'registrations' }
 
-  # Devise authentication routes
-  devise_for :users, controllers: { registrations: 'registrations' }
+    # User form
+    resources :individual_forms, only: [:show, :index, :new, :create, :edit, :update]
 
-  # User form
-  resources :individual_forms, only: [:show, :index, :new, :create, :edit, :update]
+    # User form application duo
+    resources :duo, only: [:show, :index, :new, :create, :edit, :update] do
+      resources :duo_participants, only: [:create, :destroy]
+    end
 
-  # User form application duo
-  resources :duo, only: [:show, :index, :new, :create, :edit, :update] do
-    resources :duo_participants, only: [:create, :destroy]
+    # User form application trio
+    resources :trios, only: [:index, :show, :new, :create, :edit, :update] do
+      resources :trio_participants, only: [:create, :destroy]
+    end
+
+    # User form application group
+    resources :group_forms, only: [:index, :show, :new, :create, :edit, :update] do
+      resources :participants, only: [:create, :destroy, :update]
+    end
+
+    # Info of a specific solo form
+    get '/solos/:id/info', to: 'individual_forms#info'
+
+    # Info of a specific duo form
+    get '/duos/:id/info', to: 'duo#info'
+
+    # Info of a specific trio form
+    get '/trios/:id/info', to: 'trios#info'
+
+    # Info of a specific group form
+    get '/group_forms/:id/info', to: 'group_forms#info'
+
+    # Messages routes
+    resources :messages, only: [:index, :new, :create]
   end
-
-  # User form application trio
-  resources :trios, only: [:index, :show, :new, :create, :edit, :update] do
-    resources :trio_participants, only: [:create, :destroy]
-  end
-
-  # User form application group
-  resources :group_forms, only: [:index, :show, :new, :create, :edit, :update] do
-    resources :participants, only: [:create, :destroy, :update]
-  end
-
-  # Info of a specific solo form
-  get '/solos/:id/info', to: 'individual_forms#info'
-
-  # Info of a specific duo form
-  get '/duos/:id/info', to: 'duo#info'
-
-  # Info of a specific trio form
-  get '/trios/:id/info', to: 'trios#info'
-
-  # Info of a specific group form
-  get '/group_forms/:id/info', to: 'group_forms#info'
-
-  # Messages routes
-  resources :messages, only: [:index, :new, :create]
-
 
 end
